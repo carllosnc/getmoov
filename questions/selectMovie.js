d59254ad6d0a216cf2ec43cdab58b501e7f4d663
Carlos Costa <carllos.nc@gmail.com>
2019-12-28 10:54:29 -0300

    Renaming movieDetails to selectMovies.

---
+++ questions/selectMovie.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer")
+const formats = require("../formats")
+const print = require("../print")
+
+async function movieDetails(list){
+  try {
+    const result = await inquirer.prompt([{
+      type: "list",
+      name: "movie",
+      message: `Choose the movie (${list.length} results):`,
+      choices: [...formats.formatMovieList(list)],
+      pageSize: 20
+    }])
+
+    return result.movie
+
+  } catch (error) {
+    print.errorMessage(error.message)
+  }
+}
+
+module.exports = movieDetails
