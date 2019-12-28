ea11db8e9cb19ba9ba9737b974811bb7c76bcf6d
Carlos Costa <carllos.nc@gmail.com>
2019-12-28 01:41:05 -0300

    Adding options to open the torrent.

--- questions/selectTorrent.js
+++ questions/selectTorrent.js
@@ -1,24 +1,49 @@
 const inquirer = require("inquirer")
 const shell = require("shelljs")
 const formats = require("../formats")
+const selectLegend = require("./selectLegend")
+
+function clientOptions(){
+  const options = [{
+    name: "Print Link",
+    value: "print"
+  }]
+
+  if(shell.which("qbittorrent")){
+    options.push({
+      name: "QBitTorrent",
+      value: "qbittorrent"
+    })
+  }
+
+  return options
+}
 
 async function selectTorrent(options){
   try {
-    const result = await inquirer.prompt([{
-      type: "list",
-      name: "torrent",
-      message: "Choose torrent quality:",
-      choices: options
-    }])
+    const result = await inquirer.prompt([
+      {
+        type: "list",
+        name: "torrent",
+        message: "Torrent quality:",
+        choices: options
+      },
+      {
+        type: "list",
+        name: "client",
+        message: "Torrent Client",
+        choices: clientOptions()
+      }
+    ])
 
-    const {torrent} = result
+    const {torrent, client} = result
 
-    if(shell.which("qbittorrent")){
-      shell.exec(`qbittorrent ${torrent}`)
-      formats.formatCredits()
-    }else{
+    if(client === "print"){
       formats.formatTorrentLink(torrent)
+    }else{
+      shell.exec(`${client} ${torrent}`)
     }
+
   } catch (error) {
     formats.errorMessage(error.message)
   }
