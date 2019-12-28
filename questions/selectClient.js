149f43d28470ea522e8404b5fe6ffd3ef4784bc7
Carlos Costa <carllos.nc@gmail.com>
2019-12-28 10:53:58 -0300

    Adding question to select torrent client.

---
+++ questions/selectClient.js
@@ -0,0 +1,21 @@
+const inquirer = require("inquirer")
+const print = require("../print")
+const formats = require("../formats")
+
+async function selectClient(){
+  try {
+    const result = await inquirer.prompt({
+      type: "list",
+      name: "client",
+      message: "Torrent client",
+      choices: formats.FormatTorrentClientOptions()
+    })
+
+    return result
+
+  } catch (error) {
+    print.errorMessage(error.message)
+  }
+}
+
+module.exports = selectClient
