fd7ce94e1505bf330c438e376ed4c198d3799c9b
Carlos Costa <carllos.nc@gmail.com>
2019-12-29 00:54:47 -0300

    adding print layer.

---
+++ print/index.js
@@ -0,0 +1,53 @@
+const colors = require("../colors")
+
+function errorMessage(message){
+  console.log(colors.Err(`  ${message}`))
+}
+
+function successMessage(message){
+  console.log(colors.GreenBold(`  ${message} \n`))
+}
+
+function noResultFound(){
+  console.log(colors.Err(`  No result found`))
+}
+
+function movie(movie){
+  console.log(``)
+  console.log(`  ${colors.YellowBold(movie.year)} • ${colors.CyanBold(movie.title)} `)
+  console.log(`  ${colors.Green("language:")} ${movie.language} `)
+  console.log(`  ${colors.Green("Trailer:")} https://www.youtube.com/watch?v=${movie.yt_trailer_code} `)
+  console.log(`  ${colors.Green("Cover:")} ${movie.large_cover_image} `)
+  console.log(``)
+}
+
+function logo(){
+  console.log(`
+  ▄▄▄▄▄▄▄
+  Getmoov
+  ▀▀▀▀▀▀▀
+  `)
+}
+
+function credits(){
+  console.log(`\n  Getmoov | designed and coded with ${colors.Red("♥")} \n`)
+}
+
+function torrentLink(torrent){
+  console.log(`\n  ${colors.GreenBg(` TORRENT LINK: `)} ${torrent} \n`)
+}
+
+function subtitleLink(torrent){
+  console.log(`\n  ${colors.GreenBg(` SUBTITLE LINK: `)} ${torrent} \n`)
+}
+
+module.exports = {
+  noResultFound,
+  errorMessage,
+  successMessage,
+  torrentLink,
+  subtitleLink,
+  movie,
+  credits,
+  logo
+}
