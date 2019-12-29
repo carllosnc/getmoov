092fa971ec59d8782ece29bdd274046d45f0cd3d
Carlos Costa <carllos.nc@gmail.com>
2019-12-29 00:54:33 -0300

    Adding new colors.

--- colors/index.js
+++ colors/index.js
@@ -2,18 +2,24 @@ const chalk = require("chalk")
 
 const CyanBold = text => chalk.cyan.bold(text)
 const YellowBold = text => chalk.yellow.bold(text)
-const Green = text => chalk.green.bold(text)
 const Yellow = text => chalk.yellow(text)
-const Red = text => chalk.red(text)
+const YellowBg = text => chalk.bgYellow.black(text)
+const Green = text => chalk.green(text)
+const GreenBold = text => chalk.green.bold(text)
 const GreenBg = text => chalk.bgGreen.black(text)
+const Red = text => chalk.red(text)
+const RedBg = text => chalk.bgRed.black(text)
 const Err = message => chalk.red.bold(message)
 
 module.exports = {
   CyanBold,
   YellowBold,
+  YellowBg,
   Green,
+  GreenBold,
   Yellow,
   Red,
+  RedBg,
   GreenBg,
   Err
 }
