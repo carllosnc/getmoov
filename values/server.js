28a9a593c3c9ea48a47def9a04a22eba6136723b
Carlos Costa <carllos.nc@gmail.com>
2019-12-29 00:55:38 -0300

    Adding yify subtitles.

--- values/server.js
+++ values/server.js
@@ -1,7 +1,9 @@
 const YTS = "https://yts.lt/api/v2"
 const POPCORN = "https://tv-v2.api-fetch.website"
+const YIFY_LEGENDS = "https://www.yifysubtitles.com"
 
 module.exports = {
   YTS,
-  POPCORN
+  POPCORN,
+  YIFY_LEGENDS
 }
