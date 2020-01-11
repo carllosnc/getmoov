0e6b5eafe767072a6034c91a50a7bda908a3af45
Carlos Costa <carllos.nc@gmail.com>
2020-01-11 15:03:15 -0300

    main entry

--- getmoov.js
+++ getmoov.js
@@ -1,106 +1,20 @@
 const questions = require("./src/questions")
-const formats = require("./src/formats")
-const services = require("./src/services")
-const ora = require("ora")
 const print = require("./src/print")
-const actions = require("./src/actions")
-const crawlers = require("./src/crawlers")
-
-const loadingSearch = ora("Searching movie...")
-const loadingTorrents = ora("Searching torrents...")
-const loadingSubtitles = ora("Searching subtitles...")
+const providers = require("./src/providers")
 
 print.logo()
 
 async function app() {
-  /*=========================
-    Search
-  ===========================*/
+  // Select provider
+  const providerName = await questions.selectProvider()
 
-  // Search movies
-  const searchName = await questions.searchMovies()
-
-  loadingSearch.start()
-  const moviesResponse = await services.getMovies(searchName)
-  const { movies } = moviesResponse.data
-  loadingSearch.stop()
-
-  // No result
-  if (!movies) {
-    print.errorMessage("\n  No movies found.")
-    print.credits()
-    process.exit()
+  if(providerName === "YTS"){
+    providers.yts()
   }
 
-  /*=========================
-    Movie
-  ===========================*/
-
-  // Chose the movie
-  const movie = await questions.selectMovie(movies)
-
-  // Show the movie
-  print.movie(movie)
-
-  /*=========================
-    Torrents
-  ===========================*/
-
-  // Get torrents
-  loadingTorrents.start()
-  const popCornResponse = await services.getpopcornTorrents(movie.imdb_code)
-  loadingTorrents.stop()
-
-  // Select torrent quality
-  const torrentQuality = await questions.selectTorrent([
-    ...formats.ytsTorrents(movie.torrents),
-    ...formats.popcornTorrents(popCornResponse),
-  ])
-
-  /*=========================
-    Torrent Client
-  ===========================*/
-
-  // Select torrent client
-  const client = await questions.selectTorrentClient()
-
-  // Open torrent client
-  actions.openClient(client, torrentQuality)
-
-  /*=========================
-    Subtitle
-  ===========================*/
-
-  // Get legends
-  loadingSubtitles.start()
-  const legends = await services.getSubtitles(movie.imdb_code)
-  loadingSubtitles.stop()
-
-  const filteredSubtitles = crawlers.filterSubtitles(legends)
-
-  // No subtitle found
-  if (!filteredSubtitles.length) {
-    print.errorMessage("  No subtitles found.")
-    print.credits()
-    process.exit()
+  if(providerName === "Popcorn Time"){
+    providers.popcorn()
   }
-
-  // Select legend
-  const selectedSubtitle = await questions.selectSubtitle(filteredSubtitles)
-
-  /*=========================
-    Subtitle client
-  ===========================*/
-
-  // download subtitle
-  const subtitleClient = await questions.selectSubtitleClient()
-  actions.downloadSubtitle(subtitleClient, selectedSubtitle)
-
-  /*=========================
-    Credits
-  ===========================*/
-
-  print.credits()
 }
 
 app()
