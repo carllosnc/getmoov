aeeb53160171d20c7af25bad676f4c8e8a1bd66d
Carlos Costa <carllos.nc@gmail.com>
2020-12-06 02:49:06 -0300

    Remove: client actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,28 +1,15 @@
 const print = require("../print")
-const shell = require("../shell")
 const formats = require("../formats")
-const colors = require("../colors")
 
-function openClient(client, torrent) {
-  if (client === "print") {
-    print.message(formats.torrentLink(torrent))
-  } else {
-    shell.run(`${client} ${torrent}`)
-  }
+function printTorrentLink(torrent) {
+  print.message(formats.torrentLink(torrent))
 }
 
-async function downloadSubtitle(client, subtitle) {
-  if (client === "print") {
-    return formats.subtitleLink(subtitle)
-  } else if (client === "wget") {
-    await shell.run(`wget -P ~/Downloads/ ${subtitle}`)
-    return colors.Green("\n  Subtitle saved in your download folder [ ~/Download ] \n")
-  } else {
-    shell.run(`${client} ${subtitle}`)
-  }
+function printSubtitleLink(subtitle) {
+  print.message(formats.subtitleLink(subtitle))
 }
 
 module.exports = {
-  openClient,
-  downloadSubtitle,
+  printTorrentLink,
+  printSubtitleLink,
 }
