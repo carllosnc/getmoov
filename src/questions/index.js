6a8b165759491a6354b68710dfbebbff441abd9e
Carlos Costa <carllos.nc@gmail.com>
2020-12-26 02:10:59 -0300

    Remove: selectMediaType feature.

--- src/questions/index.js
+++ src/questions/index.js
@@ -1,13 +1,10 @@
 const selectProvider = require("./selectProvider")
-const selectMediaType = require("./selectMediaType")
-
 const movies = require("./movies")
 const torrents = require("./torrents")
 const subtitle = require("./subtitle")
 
 module.exports = {
   selectProvider,
-  selectMediaType,
   ...subtitle,
   ...torrents,
   ...movies,
