ee60f588c2a147cfb9493c63b7e2ce14e8cd453d
Carlos Costa <carllos.nc@gmail.com>
2020-12-26 00:05:18 -0300

    Fix: subtitle servers.

--- src/values/server.js
+++ src/values/server.js
@@ -1,7 +1,9 @@
 const YTS = "https://yts.lt/api/v2"
-const YIFY_LEGENDS = "https://yts-subs.com"
+const YIFY_SUBS = "https://yts-subs.com"
+const YIFY_SUBS_DOWNLOAD = "https://yifysubtitles.org/subtitle"
 
 module.exports = {
   YTS,
-  YIFY_LEGENDS,
+  YIFY_SUBS,
+  YIFY_SUBS_DOWNLOAD
 }
